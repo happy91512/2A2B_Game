4969f5f4b6c4bd9b82eb800ff6985a03ce1ec0ab
happy91512 <happy6140@gmail.com>
2022-03-31 22:02:11 +0800

    import random version

--- 2A2B.py
+++ 2A2B.py
@@ -1,13 +1,34 @@
-ans_num=[]
-cin_num=[]
+import random
 
+ans_num=[]
+while len(ans_num)!=4:
+    ran=random.randint(1,9)
+    if  ran not in ans_num:
+        ans_num.append(ran)  
+    else:
+        continue  
+#make a random list for answer
+
+mes="{}A{}B"
 
 while True:
-    a=input("輸入一個四位數字")
-    if len(a)==4:
-        cin_num=list(a)
+    num_A=0
+    num_B=0
+    cin=input("輸入一個1000~9999的數字")
+    cin_num=list(cin)
+    if len(cin_num)==4:
+        for x in range(0,4):
+            if int(cin_num[x])==ans_num[x]:
+                num_A+=1
+            if int(cin_num[x]) in ans_num:
+                num_B+=1
+        num_B=num_B-num_A 
+    print(mes.format(num_A,num_B))
 
-        break
-    else:
+    if num_A==4:
+        print("恭喜答對，答案是",cin)
+        break       
+    if len(cin_num) != 4:
         print("四位數！！")
-print(cin_num)
+        continue
+    
