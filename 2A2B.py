1a1f1aece34461aac3f4439182a2a02ef8880be2
happy91512 <happy6140@gmail.com>
2022-04-25 16:22:58 +0800

    Final Version: 1. Improve the detection for repeat input. 2. Add error sign to confirm whether print the outcome.

--- 2A2B.py
+++ 2A2B.py
@@ -13,24 +13,25 @@ mes="{}A{}B"
 while True:
     num_A=0
     num_B=0
-    cin=input("輸入一個1000~9999的數字，各位數字不重複")
+    error=False
+    cin=input("輸入一個1000~9999的數字,各位數字不重複")
     cin_num=list(cin)
     if len(cin_num)==4:
-        for y in range(0,4):
-            if cin_num[y] in cin_num:
-               print("error")
-               continue
-
+        for y in range(1,4):
+            if str(cin_num[y]) in cin[:y]:
+                print("你輸入重複數字的四位數了")
+                error=True
+    if len(cin_num)==4 and error ==False:
         for x in range(0,4):
             if int(cin_num[x])==ans_num[x]:
                 num_A+=1
             if int(cin_num[x]) in ans_num:
                 num_B+=1
         num_B=num_B-num_A 
-    print(mes.format(num_A,num_B))
+        print(mes.format(num_A,num_B))
 
     if num_A==4:
-        print("恭喜答對，答案是",cin)
+        print("恭喜答對，答案是"+cin)
         break       
     if len(cin_num) != 4:
         print("四位數！！")
